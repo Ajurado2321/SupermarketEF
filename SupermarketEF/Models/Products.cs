a5da2c6290ad10b90fda9d9570be52da94996e06
Ajurado2321 <129556540+Ajurado2321@users.noreply.github.com>
2023-11-12 21:31:56 -0500

    Category and Products

--- SupermarketEF/Models/Products.cs
+++ SupermarketEF/Models/Products.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,12 @@ namespace SupermarketEF.Models
 {
     public  class Products
     {
-        
+      public int Id { get; set; }
+        public String Name { get; set; }
+        [Column(TypeName ="decimal(6,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
